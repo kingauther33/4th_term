ad69afdad6b82caed0cc9df90fe9399ec0371fc0
kingauther33 <kingauther33@gmail.com>
2022-04-08 20:48:40 +0700

    Update

--- android/practice/app/src/main/java/com/example/practice/MainActivity.java
+++ android/practice/app/src/main/java/com/example/practice/MainActivity.java
@@ -8,6 +8,7 @@ import android.os.StrictMode;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.practice.entity.Product;
 import com.example.practice.repository.AppDatabase;
@@ -44,12 +45,31 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 String productName = productInput.getText().toString();
-                int productQuantity = Integer.parseInt(quantityInput.getText().toString());
+                String productQuantityBeforeConvert = quantityInput.getText().toString();
+
+                if (productName.equals("")) {
+                    Toast toast = Toast.makeText(getApplicationContext(), "Need to provide product name", Toast.LENGTH_LONG);
+                    toast.show();
+                    return;
+                }
+
+                if (!isInteger(productQuantityBeforeConvert)) {
+                    Toast toast = Toast.makeText(getApplicationContext(), "Quantity must be whole number and cannot be null", Toast.LENGTH_LONG);
+                    toast.show();
+                    return;
+                }
+
+
+
+                int productQuantity = Integer.parseInt(productQuantityBeforeConvert);
 
                 Product product = new Product();
                 product.setName(productName);
                 product.setQuantity(productQuantity);
                 db.productDao().insertProduct(product);
+
+                Toast toast = Toast.makeText(getApplicationContext(), "Add product successfully", Toast.LENGTH_LONG);
+                toast.show();
             }
         });
 
@@ -68,5 +88,29 @@ public class MainActivity extends AppCompatActivity {
         addBtn = (Button) findViewById(R.id.add_product);
         viewBtn = (Button) findViewById(R.id.view_product);
         db = AppDatabase.getAppDatabase(this);
+    }
+
+    public static boolean isInteger(String str) {
+        if (str == null) {
+            return false;
+        }
+        int length = str.length();
+        if (length == 0) {
+            return false;
+        }
+        int i = 0;
+        if (str.charAt(0) == '-') {
+            if (length == 1) {
+                return false;
+            }
+            i = 1;
+        }
+        for (; i < length; i++) {
+            char c = str.charAt(i);
+            if (c < '0' || c > '9') {
+                return false;
+            }
+        }
+        return true;
     }
 }
